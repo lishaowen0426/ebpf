1156af34b63f0d3a2ccee667b7ba833421ee8a82
lsw <litony0426@gmail.com>
2023-10-10 17:05:50 +0900

    separate ebpf user and kernel

--- src/demo.bpf.c
+++ src/demo.bpf.c
@@ -1,24 +1,55 @@
 #include "vmlinux.h"
+#include <linux/unistd.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
+const u32 UserID = 1000;
+const u32 RootID = 0;
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, __NR_syscalls);
+    __type(key, u32);
+    __type(value, unsigned long);
+} user_syscall_counts SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, __NR_syscalls);
+    __type(key, u32);
+    __type(value, unsigned long);
+} root_syscall_counts SEC(".maps");
 
 
-
-SEC("raw_tracepoint/sys_enter")
+SEC("raw_tracepoint/sys_exit")
 int handle_syscalls(struct bpf_raw_tracepoint_args *ctx){
 
-    unsigned long syscall_id = ctx->args[1]; 
-    volatile struct user_pt_regs *regs;
-    regs = (struct user_pt_regs*)ctx->args[0];
+    u32 nr_syscall = (u32)ctx->args[1]; 
+    unsigned long init_val = 1;
+    u32 uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+    //volatile struct user_pt_regs *regs;
+    //regs = (struct user_pt_regs*)ctx->args[0];
 
-    unsigned long syscall_no;
-    bpf_probe_read_kernel(&syscall_no, sizeof(syscall_no), regs->regs + 8 );
+    unsigned long *count = NULL;
+    void* map;
+    if (uid == UserID){
+        map = &user_syscall_counts;
+    }else if (uid == RootID){
+        map = &root_syscall_counts;
+    }
 
-    
-    bpf_printk("syscall %d is issued. syscallno is %d\n", syscall_id, syscall_no);
+    if (map){
+        count = bpf_map_lookup_elem(&user_syscall_counts, &nr_syscall);
+        if(count != NULL){
+            *count += 1;
+        }else{
+            bpf_map_update_elem(&user_syscall_counts, &nr_syscall, &init_val, BPF_NOEXIST);
+        }
+    }
+
+
     
     return 0;
 }
---
+++ src/loader.c
@@ -0,0 +1,62 @@
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <bpf/libbpf.h>
+#include <linux/bpf.h>
+
+static const char *default_filename = ".output/demo.bpf.o";
+
+static int load_and_attach(const char* filename){
+    struct bpf_object* obj = NULL;
+    struct bpf_link* link = NULL;
+    struct bpf_program *prog = NULL;
+    int user_map_fd, root_map_fd;
+
+    obj = bpf_object__open_file(filename, NULL);
+    if (libbpf_get_error(obj)) {
+        fprintf(stderr, "Open bpf object file failed: %s\n", strerror(errno));
+        obj = NULL;
+        goto cleanup;
+    }
+
+    if(bpf_object__load(obj)){
+        fprintf(stderr, "Load bpf object file failed: %s\n", strerror(errno));
+        goto cleanup;
+    }
+
+    user_map_fd = bpf_object__find_map_fd_by_name(obj, "user_syscall_counts");
+    root_map_fd = bpf_object__find_map_fd_by_name(obj, "root_syscall_counts");
+    if(user_map_fd < 0 || root_map_fd < 0){
+        fprintf(stderr, "Find a map in bpf object file failed: %s\n", strerror(errno));
+        goto cleanup;
+    }
+
+    bpf_object__for_each_program(prog, obj){
+        printf("Program name: %s\n", bpf_program__name(prog));
+        link = bpf_program__attach(prog);
+        if(libbpf_get_error(link)){
+            fprintf(stderr, "bpf_program__attach failed: %s\n", strerror(errno));
+            link = NULL;
+            goto cleanup;
+        }
+    }
+
+    printf("user_syscall_counts and root_syscall_counts map id: %d %d\n", user_map_fd, root_map_fd);
+
+    sleep(10);
+
+
+
+cleanup:
+    if(link)
+        bpf_link__destroy(link);
+        
+    if(obj)
+        bpf_object__close(obj);
+    return 0;
+}
+
+int main(int argc, char **argv){
+
+    load_and_attach(default_filename);
+}
